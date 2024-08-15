88514bb0befa739fbed756075c60ed9b24accff1
brennan-jonas <167594436+brennan-jonas@users.noreply.github.com>
2024-08-15 13:47:09 -0500

    Create read_fasta.py

---
+++ read_fasta.py
@@ -0,0 +1,30 @@
+def readFile(filePath):
+    with open(filePath, 'r') as f:
+        return [l.strip() for l in f.readlines()]
+
+
+def format_fasta(lines):
+    sequences = {}
+    current_header = None
+    current_sequence = []
+
+    for line in lines:
+        if line.startswith('>'):
+            if current_header:
+                sequences[current_header] = ''.join(current_sequence)
+            current_header = line[1:]  # Remove the '>'
+            current_sequence = []
+        else:
+            current_sequence.append(line)
+
+    if current_header:
+        sequences[current_header] = ''.join(current_sequence)
+
+    return sequences
+
+
+filePath = 'test.fasta'  # edit this with your filepath
+lines = readFile(filePath)
+sequences = format_fasta(lines)
+
+print(sequences)
